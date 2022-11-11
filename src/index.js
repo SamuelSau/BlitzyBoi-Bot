d3627c99eebb83581941eebef3c5bf6a122405f0
Samuel Sau <samuel51@usf.edu>
2022-11-10 23:54:13 -0500

    added comments, added additional gateway intents

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import {config} from 'dotenv'
+import { Client, ClientPresence, GatewayIntentBits } from 'discord.js';
+import { config } from 'dotenv';
 
 //invoke config function for environment variables
 config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
 
 const TOKEN = process.env.BOT_TOKEN;
 
-client.login(TOKEN);
-
+//listens to events
+//event, callback function / event handler function
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.username}!`);
 });
+
+client.on('messageCreate', (message) => {
+	console.log(message.content);
+});
+
+client.login(TOKEN);
